7b90bb9f8cf883c54c58956f81e172a3c36fb6e0
Guido Menendez <guidomenendez0@gmail.com>
2023-03-12 22:17:40 -0300

    Se agregan Pre y Post condiciones a los metodos de la clase "Equipo".

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,10 +1,12 @@
 public class Equipo {
     private String ID_equipo;
 
+    //Post: Crea un equipo con el ID especificado
     public Equipo(String ID_equipo){
         this.ID_equipo = ID_equipo;
     }
 
+    //Post: Devuelve el ID del equipo
     public String get_id_equipo(){
         return ID_equipo;
     }
